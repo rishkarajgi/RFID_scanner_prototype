9e7977154df05db808c6eca22f9e74775472b31f
Rishabh007 <rishabh.karajgi@gmail.com>
2015-01-07 22:49:37 +0530

    created admin

---
+++ rfid/manager/migrations/0001_initial.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='AuthGroup',
+            fields=[
+                ('id', models.IntegerField(serialize=False, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=80)),
+            ],
+            options={
+                'db_table': 'auth_group',
+                'managed': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='AuthGroupPermissions',
+            fields=[
+                ('id', models.IntegerField(serialize=False, primary_key=True)),
+            ],
+            options={
+                'db_table': 'auth_group_permissions',
+                'managed': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='AuthPermission',
+            fields=[
+                ('id', models.IntegerField(serialize=False, primary_key=True)),
+                ('name', models.CharField(max_length=50)),
+                ('codename', models.CharField(max_length=100)),
+            ],
+            options={
+                'db_table': 'auth_permission',
+                'managed': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='AuthUser',
+            fields=[
+                ('id', models.IntegerField(serialize=False, primary_key=True)),
+                ('password', models.CharField(max_length=128)),
+                ('last_login', models.DateTimeField()),
+                ('is_superuser', models.IntegerField()),
+                ('username', models.CharField(unique=True, max_length=30)),
+                ('first_name', models.CharField(max_length=30)),
+                ('last_name', models.CharField(max_length=30)),
+                ('email', models.CharField(max_length=75)),
+                ('is_staff', models.IntegerField()),
+                ('is_active', models.IntegerField()),
+                ('date_joined', models.DateTimeField()),
+            ],
+            options={
+                'db_table': 'auth_user',
+                'managed': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='AuthUserGroups',
+            fields=[
+                ('id', models.IntegerField(serialize=False, primary_key=True)),
+            ],
+            options={
+                'db_table': 'auth_user_groups',
+                'managed': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='AuthUserUserPermissions',
+            fields=[
+                ('id', models.IntegerField(serialize=False, primary_key=True)),
+            ],
+            options={
+                'db_table': 'auth_user_user_permissions',
+                'managed': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='DjangoAdminLog',
+            fields=[
+                ('id', models.IntegerField(serialize=False, primary_key=True)),
+                ('action_time', models.DateTimeField()),
+                ('object_id', models.TextField(blank=True)),
+                ('object_repr', models.CharField(max_length=200)),
+                ('action_flag', models.IntegerField()),
+                ('change_message', models.TextField()),
+            ],
+            options={
+                'db_table': 'django_admin_log',
+                'managed': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='DjangoContentType',
+            fields=[
+                ('id', models.IntegerField(serialize=False, primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+                ('app_label', models.CharField(max_length=100)),
+                ('model', models.CharField(max_length=100)),
+            ],
+            options={
+                'db_table': 'django_content_type',
+                'managed': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='DjangoMigrations',
+            fields=[
+                ('id', models.IntegerField(serialize=False, primary_key=True)),
+                ('app', models.CharField(max_length=255)),
+                ('name', models.CharField(max_length=255)),
+                ('applied', models.DateTimeField()),
+            ],
+            options={
+                'db_table': 'django_migrations',
+                'managed': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='DjangoSession',
+            fields=[
+                ('session_key', models.CharField(max_length=40, serialize=False, primary_key=True)),
+                ('session_data', models.TextField()),
+                ('expire_date', models.DateTimeField()),
+            ],
+            options={
+                'db_table': 'django_session',
+                'managed': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='StudentData',
+            fields=[
+                ('userid', models.IntegerField(serialize=False, primary_key=True)),
+                ('name', models.CharField(max_length=64)),
+                ('roll_number', models.CharField(unique=True, max_length=10, blank=True)),
+                ('registration_number', models.CharField(unique=True, max_length=10, blank=True)),
+                ('current_section', models.CharField(max_length=4)),
+                ('current_year', models.CharField(max_length=4)),
+                ('joining_year', models.CharField(max_length=4)),
+                ('course', models.CharField(max_length=10)),
+                ('branch', models.CharField(max_length=10)),
+                ('gender', models.CharField(max_length=1)),
+                ('email', models.CharField(unique=True, max_length=64)),
+                ('birthday', models.DateField()),
+                ('country', models.CharField(max_length=32)),
+                ('mobile', models.CharField(max_length=16)),
+                ('emergency_contact', models.CharField(max_length=16)),
+                ('sbh_account', models.CharField(max_length=32, blank=True)),
+                ('passport', models.CharField(max_length=20, blank=True)),
+                ('hostel_room', models.CharField(max_length=10)),
+                ('hostel', models.CharField(max_length=10)),
+                ('mess', models.CharField(max_length=10)),
+                ('created_location', models.CharField(max_length=32)),
+                ('created_time', models.DateTimeField()),
+            ],
+            options={
+                'db_table': 'student_data',
+                'managed': False,
+            },
+            bases=(models.Model,),
+        ),
+    ]
