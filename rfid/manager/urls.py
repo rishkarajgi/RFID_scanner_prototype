129c75d1e6bdbead1c3f5652ead9207cf6bed73c
makalaaneesh <makalaaneesh@yahoo.com>
2015-01-10 01:04:23 +0530

    roll number details

---
+++ rfid/manager/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+from manager import views
+
+urlpatterns= patterns('',
+	url(r'^index', views.index, name='index'),
+	url(r'^(?P<rollnumber>\d+)/$', views.detail, name='detail'),
+	url(r'^entered/', views.entered, name='entered'),
+	)
--- rfid/rfid/urls.py
+++ rfid/rfid/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'rfid.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^manager/', include('manager.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
