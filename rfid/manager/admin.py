bbdbd84159532d9a5d0eacd2f2c525218f601383
karajrish <rishabh.karajgi@gmail.com>
2015-11-25 22:10:53 +0530

    deleted trash

--- rfid/manager/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from manager.models import StudentData
-
-admin.site.register(StudentData)
-
-# Register your models here.
--- rfid/manager/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-from manager import views
-
-urlpatterns= patterns('',
-	url(r'^index', views.index, name='index'),
-	url(r'^(?P<rollnumber>\d+)/$', views.detail, name='detail'),
-	url(r'^entered/', views.entered, name='entered'),
-	url(r'^allstudents/', views.allstudents, name='allstudents'),
-	)
